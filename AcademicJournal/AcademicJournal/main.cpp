58b7fbc919d8e63127372ddb5da6277a76a2f439
Anton Velikodnyy <30283914+Anton-Velikodnyy@users.noreply.github.com>
2017-08-18 11:29:39 -0700

    attemp2

---
+++ AcademicJournal/AcademicJournal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AcademicJournal
+//
+//  Created by Anton Velikodnyy on 8/18/17.
+//  Copyright © 2017 Anton Velikodnyy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
